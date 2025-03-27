60adf746ec129c1830c2906ee35bda3affbdeb06
cloudwebrtc <duanweiwei1982@gmail.com>
2025-03-27 10:56:12 +0800

    fix layout for media select button.

--- lib/src/ui/widgets/room/media_device_select_button.dart
+++ lib/src/ui/widgets/room/media_device_select_button.dart
@@ -51,84 +51,85 @@ class MediaDeviceSelectWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var deviceScreenType = getDeviceType(MediaQuery.of(context).size);
     return Row(mainAxisSize: MainAxisSize.min, children: [
-      ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor: WidgetStateProperty.all(
-              deviceIsOpened ? LKColors.lkBlue : Colors.grey.withOpacity(0.9)),
-          foregroundColor: WidgetStateProperty.all(Colors.white),
-          overlayColor: WidgetStateProperty.all(
-              deviceIsOpened ? LKColors.lkLightBlue : Colors.grey),
-          shape: WidgetStateProperty.all(const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  bottomLeft: Radius.circular(20.0)))),
-          padding: WidgetStateProperty.all(
-            deviceScreenType == DeviceScreenType.desktop ||
-                    lkPlatformIsDesktop()
-                ? const EdgeInsets.fromLTRB(10, 20, 10, 20)
-                : const EdgeInsets.all(12),
+      SizedBox(
+        height: 40,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(deviceIsOpened
+                ? LKColors.lkBlue
+                : Colors.grey.withOpacity(0.9)),
+            foregroundColor: WidgetStateProperty.all(Colors.white),
+            overlayColor: WidgetStateProperty.all(
+                deviceIsOpened ? LKColors.lkLightBlue : Colors.grey),
+            shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    bottomLeft: Radius.circular(20.0)))),
+            padding: WidgetStateProperty.all(
+                const EdgeInsets.fromLTRB(12, 20, 12, 20)),
           ),
-        ),
-        onPressed: () => onToggle?.call(!deviceIsOpened),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(deviceIsOpened || defaultSelectable ? iconOn : iconOff),
-            const SizedBox(width: 2),
-            if (title != null &&
-                (deviceScreenType != DeviceScreenType.mobile || showLabel))
-              Text(
-                title!,
-                style: TextStyle(fontSize: 14),
-              ),
-          ],
+          onPressed: () => onToggle?.call(!deviceIsOpened),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(deviceIsOpened || defaultSelectable ? iconOn : iconOff),
+              const SizedBox(width: 2),
+              if (title != null &&
+                  (deviceScreenType != DeviceScreenType.mobile || showLabel))
+                Text(
+                  title!,
+                  style: TextStyle(fontSize: 14),
+                ),
+            ],
+          ),
         ),
       ),
       const SizedBox(width: 0.2),
-      PopupMenuButton<MediaDevice>(
-        padding: const EdgeInsets.all(12),
-        icon: const Icon(Icons.arrow_drop_down),
-        offset: Offset(
-            0, ((deviceList.isNotEmpty ? deviceList.length : 1) * -55.0)),
-        style: ButtonStyle(
-          backgroundColor:
-              WidgetStateProperty.all(Colors.grey.withOpacity(0.9)),
-          foregroundColor: WidgetStateProperty.all(Colors.white),
-          overlayColor: WidgetStateProperty.all(Colors.grey),
-          elevation: WidgetStateProperty.all(20),
-          shape: WidgetStateProperty.all(const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(20.0),
-                  bottomRight: Radius.circular(20.0)))),
+      SizedBox(
+        height: 40,
+        child: PopupMenuButton<MediaDevice>(
+          icon: const Icon(Icons.arrow_drop_down),
+          offset: Offset(
+              0, ((deviceList.isNotEmpty ? deviceList.length : 1) * -55.0)),
+          style: ButtonStyle(
+            backgroundColor:
+                WidgetStateProperty.all(Colors.grey.withOpacity(0.9)),
+            foregroundColor: WidgetStateProperty.all(Colors.white),
+            overlayColor: WidgetStateProperty.all(Colors.grey),
+            shape: WidgetStateProperty.all(const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0)))),
+          ),
+          enabled: deviceIsOpened || defaultSelectable,
+          itemBuilder: (BuildContext context) {
+            return [
+              if (deviceList.isNotEmpty)
+                ...deviceList.map((device) {
+                  return PopupMenuItem<MediaDevice>(
+                    value: device,
+                    child: ListTile(
+                      selected: (device.deviceId == selectedDeviceId),
+                      selectedColor: LKColors.lkBlue,
+                      leading: (device.deviceId == selectedDeviceId)
+                          ? Icon(
+                              Icons.check_box_outlined,
+                              color: (device.deviceId == selectedDeviceId)
+                                  ? LKColors.lkBlue
+                                  : Colors.white,
+                            )
+                          : const Icon(
+                              Icons.check_box_outline_blank,
+                              color: Colors.white,
+                            ),
+                      title: Text(device.label),
+                    ),
+                    onTap: () => onSelect?.call(device),
+                  );
+                })
+            ];
+          },
         ),
-        enabled: deviceIsOpened || defaultSelectable,
-        itemBuilder: (BuildContext context) {
-          return [
-            if (deviceList.isNotEmpty)
-              ...deviceList.map((device) {
-                return PopupMenuItem<MediaDevice>(
-                  value: device,
-                  child: ListTile(
-                    selected: (device.deviceId == selectedDeviceId),
-                    selectedColor: LKColors.lkBlue,
-                    leading: (device.deviceId == selectedDeviceId)
-                        ? Icon(
-                            Icons.check_box_outlined,
-                            color: (device.deviceId == selectedDeviceId)
-                                ? LKColors.lkBlue
-                                : Colors.white,
-                          )
-                        : const Icon(
-                            Icons.check_box_outline_blank,
-                            color: Colors.white,
-                          ),
-                    title: Text(device.label),
-                  ),
-                  onTap: () => onSelect?.call(device),
-                );
-              })
-          ];
-        },
       ),
     ]);
   }
