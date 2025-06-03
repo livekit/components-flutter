b601ce388cd68f6e410a2e88d8ad6ec336d56ab6
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-06-03 22:13:16 +0900

    First attempt

--- lib/src/ui/widgets/track/audio_visualizer_widget.dart
+++ lib/src/ui/widgets/track/audio_visualizer_widget.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/material.dart';
+import 'dart:math' show max;
 
-import 'package:livekit_client/livekit_client.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:livekit_client/livekit_client.dart' as sdk;
 import 'package:provider/provider.dart';
 
 import '../../../context/track_reference_context.dart';
-import 'no_track_widget.dart';
+import '../../../types/agent_state.dart';
 
 class AudioVisualizerWidgetOptions {
   final int barCount;
@@ -34,57 +36,39 @@ class AudioVisualizerWidgetOptions {
 
 class AudioVisualizerWidget extends StatelessWidget {
   final AudioVisualizerWidgetOptions options;
-  final Widget noTrackWidget;
   final Color backgroundColor;
 
   const AudioVisualizerWidget({
     Key? key,
-    this.noTrackWidget = const NoTrackWidget(),
     this.backgroundColor = Colors.transparent,
     this.options = const AudioVisualizerWidgetOptions(),
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    var trackCtx = Provider.of<TrackReferenceContext?>(context);
-
-    if (trackCtx == null) {
-      return noTrackWidget;
-    }
-
-    return Consumer<TrackReferenceContext>(
-      builder: (context, trackCtx, child) =>
-          Selector<TrackReferenceContext, AudioTrack?>(
-        selector: (context, audioTrack) => trackCtx.audioTrack,
-        builder: (BuildContext context, AudioTrack? audioTrack, Widget? child) {
-          if (trackCtx.audioTrack == null) {
-            return noTrackWidget;
-          }
-          return Container(
+  Widget build(BuildContext context) => Builder(
+        builder: (context) => Consumer<TrackReferenceContext?>(
+          builder: (BuildContext context, TrackReferenceContext? trackCtx, Widget? child) => Container(
             color: backgroundColor,
-            child: Center(
-              child: SoundWaveformWidget(
-                audioTrack: audioTrack,
-                participant: trackCtx.participant,
-                options: options,
-              ),
+            child: SoundWaveformWidget(
+              key: ValueKey('SoundWaveformWidget-${trackCtx?.participant.sid}-${trackCtx?.audioTrack?.sid}'),
+              audioTrack: trackCtx?.audioTrack,
+              participant: trackCtx?.participant,
+              options: options,
             ),
-          );
-        },
-      ),
-    );
-  }
+          ),
+        ),
+      );
 }
 
 class SoundWaveformWidget extends StatefulWidget {
+  final sdk.Participant? participant;
+  final sdk.AudioTrack? audioTrack;
   final AudioVisualizerWidgetOptions options;
-  final AudioTrack? audioTrack;
-  final Participant participant;
 
   const SoundWaveformWidget({
     super.key,
+    this.participant,
     this.audioTrack,
-    required this.participant,
     this.options = const AudioVisualizerWidgetOptions(),
   });
 
@@ -92,53 +76,73 @@ class SoundWaveformWidget extends StatefulWidget {
   State<SoundWaveformWidget> createState() => _SoundWaveformWidgetState();
 }
 
-class _SoundWaveformWidgetState extends State<SoundWaveformWidget>
-    with TickerProviderStateMixin {
-  late AnimationController controller;
-  late List<double> samples;
-  AudioVisualizer? _visualizer;
-  EventsListener<AudioVisualizerEvent>? _listener;
-  EventsListener<ParticipantEvent>? _participantListener;
+const agentStateAttributeKey = 'lk.agent.state';
 
-  void _startVisualizer(AudioTrack? track) async {
-    if (track == null) {
-      return;
+class _SoundWaveformWidgetState extends State<SoundWaveformWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _pulseAnimation;
+  List<double> samples = [];
+
+  sdk.AudioVisualizer? _visualizer;
+  sdk.EventsListener<sdk.AudioVisualizerEvent>? _visualizerListener;
+  sdk.EventsListener<sdk.ParticipantEvent>? _participantListener;
+
+  // Agent support
+  AgentState _agentState = AgentState.initializing;
+
+  @override
+  void didUpdateWidget(SoundWaveformWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.participant?.sid != widget.participant?.sid ||
+        oldWidget.audioTrack?.sid != widget.audioTrack?.sid ||
+        oldWidget.options != widget.options) {
+      // Re-attach listeners
+      _detachListeners();
+      _attachListeners();
     }
-
-    _participantListener ??= widget.participant.createListener();
-    _participantListener?.on<TrackMutedEvent>((e) {
-      if (mounted) {
-        setState(() {
-          samples = List.filled(widget.options.barCount,
-              widget.options.minHeight / widget.options.maxHeight);
-        });
-      }
-    });
-
-    samples = List.filled(widget.options.barCount,
-        widget.options.minHeight / widget.options.maxHeight);
-    _visualizer ??= createVisualizer(track,
-        options: AudioVisualizerOptions(
-            barCount: widget.options.barCount,
-            centeredBands: widget.options.centeredBands));
-    _listener ??= _visualizer?.createListener();
-    _listener?.on<AudioVisualizerEvent>((e) {
-      if (mounted) {
-        setState(() {
-          samples = e.event.map((e) => ((e as num) * 100).toDouble()).toList();
-        });
-      }
-    });
-
-    await _visualizer!.start();
   }
 
-  void _stopVisualizer() async {
+  Future<void> _attachListeners() async {
+    if (widget.participant != null) {
+      _participantListener = widget.participant!.createListener();
+      _participantListener?.on<sdk.TrackMutedEvent>((e) {
+        if (!mounted) return;
+        setState(() {
+          samples = List.filled(widget.options.barCount, 0.0);
+        });
+      });
+
+      _participantListener?.on<sdk.ParticipantAttributesChanged>((e) {
+        if (!mounted) return;
+        final agentStateString = e.attributes[agentStateAttributeKey];
+        setState(() {
+          _agentState = agentStateString != null ? AgentState.fromString(agentStateString) : AgentState.initializing;
+        });
+      });
+    }
+
+    if (widget.audioTrack != null) {
+      _visualizer = sdk.createVisualizer(widget.audioTrack!,
+          options: sdk.AudioVisualizerOptions(
+              barCount: widget.options.barCount, centeredBands: widget.options.centeredBands));
+      _visualizerListener = _visualizer?.createListener();
+      _visualizerListener?.on<sdk.AudioVisualizerEvent>((e) {
+        if (!mounted) return;
+        setState(() {
+          samples = e.event.map((e) => ((e as num)).toDouble()).toList();
+        });
+      });
+
+      await _visualizer!.start();
+    }
+  }
+
+  Future<void> _detachListeners() async {
     await _visualizer?.stop();
     await _visualizer?.dispose();
     _visualizer = null;
-    await _listener?.dispose();
-    _listener = null;
+    await _visualizerListener?.dispose();
+    _visualizerListener = null;
     await _participantListener?.dispose();
     _participantListener = null;
   }
@@ -147,69 +151,109 @@ class _SoundWaveformWidgetState extends State<SoundWaveformWidget>
   void initState() {
     super.initState();
 
-    controller = AnimationController(
-        vsync: this,
-        duration: Duration(
-          milliseconds: widget.options.durationInMilliseconds,
-        ))
-      ..repeat();
+    samples = List.filled(widget.options.barCount, 0.0);
 
-    _startVisualizer(widget.audioTrack);
+    _controller = AnimationController(
+      duration: Duration(milliseconds: widget.options.durationInMilliseconds),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _pulseAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    _attachListeners();
   }
 
   @override
   void dispose() {
-    controller.dispose();
-    _stopVisualizer();
+    _controller.dispose();
+    _detachListeners();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final count = widget.options.barCount;
-    final minHeight = widget.options.minHeight;
-    final maxHeight = widget.options.maxHeight;
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) =>
-          AnimatedBuilder(
-        animation: controller,
-        builder: (c, child) {
+    // Get center index
+    final centerIndex = (samples.length / 2).floor();
+
+    return AnimatedBuilder(
+        animation: _pulseAnimation,
+        builder: (ctx, _) {
+          // Listening state
+          if (_agentState == AgentState.initializing || _agentState == AgentState.listening) {
+            final elements = List.generate(
+              samples.length,
+              (i) => BarsViewItem(
+                  value: samples[i],
+                  color: i == centerIndex
+                      ? widget.options.color.withValues(alpha: 0.1 + (_pulseAnimation.value - 0.1))
+                      : widget.options.color.withValues(alpha: 0.1)),
+            );
+            return BarsView(
+              options: widget.options,
+              elements: elements,
+            );
+          }
+
+          final elements = List.generate(
+            samples.length,
+            (i) => BarsViewItem(value: samples[i], color: widget.options.color),
+          );
+          return BarsView(
+            options: widget.options,
+            elements: elements,
+          );
+        });
+  }
+}
+
+class BarsViewItem {
+  final double value;
+  final Color color;
+
+  BarsViewItem({
+    required this.value,
+    required this.color,
+  });
+}
+
+class BarsView extends StatelessWidget {
+  final AudioVisualizerWidgetOptions options;
+  final List<BarsViewItem> elements;
+
+  const BarsView({
+    super.key,
+    required this.options,
+    required this.elements,
+  });
+
+  @override
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (context, constraints) {
+          final delta = (constraints.maxWidth / elements.length) - options.spacing;
+
           return Row(
             mainAxisSize: MainAxisSize.min,
-            children: List.generate(
-              count,
-              (i) {
-                final heightPercent =
-                    ((samples[i] - minHeight) / (maxHeight - minHeight))
-                        .clamp(0.0, 1.0);
-                final barOpacity =
-                    (1.0 - widget.options.barMinOpacity) * heightPercent +
-                        widget.options.barMinOpacity;
-
-                return AnimatedContainer(
-                  duration: Duration(
-                      milliseconds:
-                          widget.options.durationInMilliseconds ~/ count),
-                  margin: i == (samples.length - 1)
-                      ? EdgeInsets.zero
-                      : EdgeInsets.only(right: widget.options.spacing),
-                  height: samples[i] < minHeight
-                      ? minHeight
-                      : samples[i] > maxHeight
-                          ? maxHeight
-                          : samples[i],
-                  width: widget.options.width,
-                  decoration: BoxDecoration(
-                    color: widget.options.color.withValues(alpha: barOpacity),
-                    borderRadius:
-                        BorderRadius.circular(widget.options.cornerRadius),
+            spacing: options.spacing,
+            children: elements
+                .mapIndexed(
+                  (index, element) => Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: options.durationInMilliseconds ~/ options.barCount),
+                      decoration: BoxDecoration(
+                        color: element.color,
+                        borderRadius: BorderRadius.circular(options.cornerRadius),
+                      ),
+                      height: max(delta, (element.value * (constraints.maxHeight - delta)) + delta),
+                    ),
                   ),
-                );
-              },
-            ),
+                )
+                .toList(),
           );
         },
-      ),
-    );
-  }
+      );
 }
