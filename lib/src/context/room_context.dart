458ab99c56c0c281976cb3cd8c5f8547445b4813
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-12-07 19:26:22 +0900

    v1.2.3 (#42)

--- lib/src/context/room_context.dart
+++ lib/src/context/room_context.dart
@@ -145,7 +145,7 @@ class RoomContext extends ChangeNotifier with ChatContextMixin, TranscriptionCon
     if (connect && url != null && token != null) {
       _url = url;
       _token = token;
-      unawaited(this.connect(url: url, token: token).catchError((error, stack) {
+      unawaited(this.connect(url: url, token: token).catchError((Object error, StackTrace stack) {
         Debug.event('RoomContext: auto connect failed: $error');
         // Preserve original behavior by rethrowing after logging.
         throw error;
